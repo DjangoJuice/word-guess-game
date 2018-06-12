7190a810809d602227013725b0a31c19022aa2ce
Mike Stewart <mike@Mikes-MacBook-Pro.local>
2018-06-12 13:05:15 -0400

    HTML Framing, JS Var's

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var currentWinsTotal = 0
+var currentCountryGuess
+var previouslyGuessedLetter = []
+var remainingGuessesTotal = 10
+var arrayOfCountries = [America, Canada, Scotland, Ireland, Italy, France, Russia, China, Japan, England, Mexico, Venezuela, Argentina]
+
